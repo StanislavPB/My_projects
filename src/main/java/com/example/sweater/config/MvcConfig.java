acd99f4ba3929952244833a1d2b3bae99329a3ef
StanislavPB <perminovsb@gmail.com>
2022-05-08 15:20:15 +0300

    .

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -1,14 +1,24 @@
 package com.example.sweater.config;
 
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = MvcConfig.class)
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("loginPage");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void configureViewResolvers (ViewResolverRegistry registry) {
+        registry.freeMarker();
     }
 }
